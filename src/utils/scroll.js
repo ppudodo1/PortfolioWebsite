4eb0ae5aeff0213bb7a11d32bb0932bcf0163fae
ppudodo <dkatavic03@gmail.com>
2024-04-13 15:12:05 +0200

    Changed contact info

--- src/utils/scroll.js
+++ src/utils/scroll.js
@@ -4,8 +4,6 @@ export function animate() {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
             entry.target.classList.add('show');
-          } else {
-            entry.target.classList.remove('show');
           }
         });
       },
@@ -18,22 +16,5 @@ export function animate() {
     const hiddenElements = document.querySelectorAll('.hidden');
 
     hiddenElements.forEach((el) => observer.observe(el));
-    const observer2 = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              entry.target.classList.add('show2');
-            } else {
-              entry.target.classList.remove('show2');
-            }
-          });
-        },
-        {
-          root: null, // Watch for intersection changes in relation to the viewport
-          threshold: 0.5, // Trigger when 50% of the element is visible
-        }
-      );
     
-      const hiddenElements2 = document.querySelectorAll('.hidden2');
-      hiddenElements2.forEach((el) => observer2.observe(el));
   }
